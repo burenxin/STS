5400841b797aec629d6cc29693b07c40c5602795
lutongtong <851945356@qq.com>
2018-11-30 14:38:11 +0800

    项目建立

---
+++ src/test/java/cn/jzt56/single_ticket_system/SingleTicketSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.jzt56.single_ticket_system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SingleTicketSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
