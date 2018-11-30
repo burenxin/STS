a3f996c65e8c6bbf732e8bccad84995eaf79b42f
lutongtong <851945356@qq.com>
2018-11-30 15:47:24 +0800

    项目建立

---
+++ src/main/java/cn/jzt56/singleticketsystem/SingleticketsystemApplication.java
@@ -0,0 +1,12 @@
+package cn.jzt56.singleticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleticketsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleticketsystemApplication.class, args);
+	}
+}
