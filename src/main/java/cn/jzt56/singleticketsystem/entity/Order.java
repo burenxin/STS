1b74d1de2299f4b4c9b0713888ae8d587e64e140
lutongtong <851945356@qq.com>
2018-12-06 16:27:18 +0800

    修改字段

--- src/main/java/cn/jzt56/singleticketsystem/entity/Order.java
+++ src/main/java/cn/jzt56/singleticketsystem/entity/Order.java
@@ -14,8 +14,8 @@ public class Order implements Serializable{
     private String consignor;//寄件人姓名
     private String phoneNumConsignee;//收件人电话
     private String consignee;//收件人名
-    private String starArea;//起始地址
-    private String starAreaMx;//起始地街道
+    private String startArea;//起始地址
+    private String startAreaMx;//起始地街道
     private String endArea;//目的地
     private String endAreaMx;//目的地街道
     private String goodsType;//商品类型
@@ -24,7 +24,7 @@ public class Order implements Serializable{
     private String weight;//重量
     private String deliveryTime;//提货时间
     private String receivingTime;//预估收货时间
-    private BigDecimal tansportPrices;//运输费用
+    private BigDecimal transportPrices;//运输费用
     private String status;//状态
     private String createdTime;//创建时间
     private String updatedTime;//更新时间
